a8b795b799e0431fe83165655a7ca8eedb7c2eac
Thomas Pfau <github@thomaspfau.de>
2022-02-02 12:06:40 +0200

    Adding Test Class for authentication

---
+++ src/test/java/fi/abo/kogni/soile2/http_server/authentication/SoileAuthenticationTest.java
@@ -0,0 +1,10 @@
+package fi.abo.kogni.soile2.http_server.authentication;
+
+import org.junit.runner.RunWith;
+
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+
+@RunWith(VertxUnitRunner.class)
+public class SoileAuthenticationTest {
+
+}
