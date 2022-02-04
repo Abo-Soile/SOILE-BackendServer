6054a666d837bf7a830f288d6fb2332629996829
Thomas Pfau <github@thomaspfau.de>
2022-02-04 09:45:40 +0200

    Updates

--- src/main/java/fi/abo/kogni/soile2/http_server/userManagement/exceptions/UserDoesNotExistException.java
+++ src/main/java/fi/abo/kogni/soile2/http_server/userManagement/exceptions/UserDoesNotExistException.java
@@ -2,6 +2,7 @@ package fi.abo.kogni.soile2.http_server.userManagement.exceptions;
 
 public class UserDoesNotExistException extends Exception{	
 
+	public static int ERRORCODE = 1001;
 	public UserDoesNotExistException(String username) {
 		super("User " + username + " does not exist" );
 	}
