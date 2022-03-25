b5af24d1c12a4d1edb11fc04040101f1624d01b5
Thomas Pfau <github@thomaspfau.de>
2022-03-25 06:52:42 +0200

    Adding Security tilities

---
+++ src/main/java/fi/abo/kogni/soile2/security/PasswordUtils.java
@@ -0,0 +1,37 @@
+package fi.abo.kogni.soile2.security;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class PasswordUtils {
+
+	
+	public static String decrypt(String key, String encrypted)
+	{
+		
+	}
+	
+	public static SecretKey getKeyFromPassword(String password)
+		    throws NoSuchAlgorithmException, InvalidKeySpecException {
+		    
+		    SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+		    KeySpec spec = new PBEKeySpec(password.toCharArray());
+		    SecretKey secret = new SecretKeySpec(factory.generateSecret(spec)
+		        .getEncoded(), "AES");
+		    return secret;
+		}
+	
+	public static IvParameterSpec generateIv() {
+	    byte[] iv = new byte[16];
+	    new SecureRandom().nextBytes(iv);
+	    return new IvParameterSpec(iv);
+	}
+}
