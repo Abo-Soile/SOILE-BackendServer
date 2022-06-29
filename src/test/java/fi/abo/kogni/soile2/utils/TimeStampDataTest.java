f1abf0d78d060224d1f74f21d754ab12faaf6c60
Thomas Pfau <github@thomaspfau.de>
2022-06-29 09:03:52 +0300

    Tests for utils

---
+++ src/test/java/fi/abo/kogni/soile2/utils/TimeStampDataTest.java
@@ -0,0 +1,42 @@
+package fi.abo.kogni.soile2.utils;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import fi.abo.kogni.soile2.http_server.MongoTestBase;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+
+@RunWith(VertxUnitRunner.class)
+public class TimeStampDataTest extends MongoTestBase{
+
+	private String coll = "testCollection";
+	
+	@Before
+	public void initDB(TestContext context)
+	{
+		final Async oasync = context.async();
+		mongo_client.save(coll, new JsonObject().put("id", 12)).onSuccess(res 
+				-> {
+					oasync.complete();
+				}).onFailure(res -> 
+				{
+					fail(res.getCause().getMessage());
+				});
+	
+	}
+	
+	
+	@Test
+	public void TestTTL() {
+		
+	}
+	
+	
+	
+}
