592a7bea8fbe4661fc70460574b3b4dd776d0e20
Thomas Pfau <github@thomaspfau.de>
2023-03-27 11:02:24 +0300

    Fixing Test so that it actually checks things

--- src/test/java/fi/abo/kogni/soile2/http_server/SoileSetupServerTest.java
+++ src/test/java/fi/abo/kogni/soile2/http_server/SoileSetupServerTest.java
@@ -11,7 +11,6 @@ import fi.abo.kogni.soile2.utils.SoileConfigLoader;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 
@@ -28,6 +27,52 @@ public class SoileSetupServerTest extends GitTest{
 		setupVertx.deployVerticle(new SetupServer(null), new DeploymentOptions())
 		.onSuccess(Void -> 
 		{			
+			System.out.println("Server set up, retrieving information");
+			mongo_client.find(SoileConfigLoader.getDbCfg().getString("userCollection"), new JsonObject())
+			.onSuccess(res -> {
+				if(res.size() != 1)
+				{
+					context.fail("Expected exactly one user to have been set up");
+				}
+				else
+				{
+					context.assertEquals(setupConf.getString("adminuser"), res.get(0).getString("username"));
+					mongo_client.find(SoileConfigLoader.getDbCfg().getString("projectInstanceCollection"), new JsonObject())
+					.onSuccess(projectRes -> {
+						context.assertEquals(2,projectRes.size());	
+						System.out.println(projectRes);
+						serverSetupAsync.complete();	
+					})					
+					.onFailure(err -> context.fail(err));															
+				}
+			})
+			.onFailure(err -> context.fail(err));
+		})
+		.onFailure(err -> context.fail(err));
+		}
+		catch(IOException e)
+		{
+			context.fail(e);
+		}
+	}
+	
+	
+	@Test
+	public void testReSetup(TestContext context){
+		Async serverSetupAsync = context.async();
+		try {
+			Vertx setupVertx = Vertx.vertx();
+		JsonObject setupConf = new JsonObject(Files.readString(Paths.get(SoileSetupServerTest.class.getClassLoader().getResource("setup.json").getPath())));
+		// for testing there is no specific data Folder
+		setupVertx.deployVerticle(new SetupServer(null), new DeploymentOptions())
+		.compose( initID -> 
+		{	
+			return setupVertx.undeploy(initID).map(initID);
+		})
+		.compose(Void -> {
+				return setupVertx.deployVerticle(new SetupServer(null), new DeploymentOptions());
+			})
+			.onSuccess(serverReStarted -> {
 			System.out.println("Server set up, retrieving information");
 			mongo_client.find(SoileConfigLoader.getDbCfg().getString("userCollection"), new JsonObject())
 			.onSuccess(res -> {
