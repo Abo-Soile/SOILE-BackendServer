7592575e29aa322e21f8140da6ad63751878ea30
Thomas Pfau <github@thomaspfau.de>
2025-10-23 10:00:29 +0300

    Always send the mails from localhost

--- src/main/java/fi/abo/kogni/soile2/http_server/auth/PasswordReset.java
+++ src/main/java/fi/abo/kogni/soile2/http_server/auth/PasswordReset.java
@@ -33,22 +33,24 @@ import jakarta.mail.internet.MimeMessage;
  * @author Thomas Pfau
  *
  */
-public class PasswordReset extends SoileAuthHandler{
+public class PasswordReset extends SoileAuthHandler {
 	static final Logger LOGGER = LogManager.getLogger(PasswordReset.class);
 
 	private MongoClient client;
 	private Vertx vertx;
 	private SoileCookieCreationHandler cookieHandler;
-	private int resettime = 60 * 60 * 1000; // 1 hour ttl for reset tokens. 
+	private int resettime = 60 * 60 * 1000; // 1 hour ttl for reset tokens.
 
 	/**
 	 * Default constructor
-	 * @param client a Mongo client to retrieve data from the db
-	 * @param tokenCreator a jwt Token Creator for auth purposes
-	 * @param vertx the Vertx Instance
+	 * 
+	 * @param client        a Mongo client to retrieve data from the db
+	 * @param tokenCreator  a jwt Token Creator for auth purposes
+	 * @param vertx         the Vertx Instance
 	 * @param cookieHandler a cookie handler for authentication purposes
 	 */
-	public PasswordReset(MongoClient client, JWTTokenCreator tokenCreator, Vertx vertx, SoileCookieCreationHandler cookieHandler) {
+	public PasswordReset(MongoClient client, JWTTokenCreator tokenCreator, Vertx vertx,
+			SoileCookieCreationHandler cookieHandler) {
 		super(cookieHandler, tokenCreator);
 		this.client = client;
 		this.vertx = vertx;
@@ -65,110 +67,106 @@ public class PasswordReset extends SoileAuthHandler{
 		return promise.future();
 	}
 
-	private Future<Void> checkAndCleanUpTokenForUser(JsonObject userData)
-	{
+	private Future<Void> checkAndCleanUpTokenForUser(JsonObject userData) {
 		Promise<Void> promise = Promise.<Void>promise();
-		String tokenCollection = SoileConfigLoader.getdbProperty("tokenCollection"); 
+		String tokenCollection = SoileConfigLoader.getdbProperty("tokenCollection");
 		String username = userData.getString(SoileConfigLoader.getUserdbField("usernameField"));
 		String usernameField = SoileConfigLoader.getUserdbField("usernameField");
 		LOGGER.debug("Checking for existing tokens for user.");
 		this.client.find(tokenCollection, new JsonObject().put(usernameField, username))
-		.onSuccess(res -> {
-			// a token already exists
-			if(res.size() > 0)
-			{								
-				JsonObject item = res.get(0);
-				if((System.currentTimeMillis() - item.getInteger("timestamp")) > resettime)
-				{
-					this.client.removeDocuments(tokenCollection, userData)
-					.onSuccess(done -> promise.complete())
-					.onFailure(err -> {
-						LOGGER.error("Unable to clean up tokens");
-						LOGGER.error(err);
-						promise.fail(err);
-					});
-				}
-				else {
-					promise.fail("Token Already exists");
-				}
-				
-			}
-			else {
-				// nothing found, great. Just return
-				promise.complete();
-			}
-		});
-		
+				.onSuccess(res -> {
+					// a token already exists
+					if (res.size() > 0) {
+						JsonObject item = res.get(0);
+						if ((System.currentTimeMillis() - item.getInteger("timestamp")) > resettime) {
+							this.client.removeDocuments(tokenCollection, userData)
+									.onSuccess(done -> promise.complete())
+									.onFailure(err -> {
+										LOGGER.error("Unable to clean up tokens");
+										LOGGER.error(err);
+										promise.fail(err);
+									});
+						} else {
+							promise.fail("Token Already exists");
+						}
+
+					} else {
+						// nothing found, great. Just return
+						promise.complete();
+					}
+				});
+
 		return promise.future();
 	}
-	
+
 	private void generateUniqueToken(Promise<String> fut, JsonObject userData) {
 		// First, check, if there is already a recent request for this user
-		// we will not generate multiple tokens for a user, thus making sure 
-		//that you cannot spam a user with reset mails 
+		// we will not generate multiple tokens for a user, thus making sure
+		// that you cannot spam a user with reset mails
 		String username = userData.getString(SoileConfigLoader.getUserdbField("usernameField"));
 		String usernameField = SoileConfigLoader.getUserdbField("usernameField");
-		String tokenCollection = SoileConfigLoader.getdbProperty("tokenCollection"); 
+		String tokenCollection = SoileConfigLoader.getdbProperty("tokenCollection");
 		LOGGER.debug("Trying to check for token");
 		this.checkAndCleanUpTokenForUser(userData)
-		.onSuccess(yay -> {
-			java.security.SecureRandom random = new java.security.SecureRandom();
-			byte[] tokenBytes = new byte[64];
-			random.nextBytes(tokenBytes);
-			String token = java.util.Base64.getUrlEncoder().withoutPadding().encodeToString(tokenBytes);		
-			JsonObject query = new JsonObject().put("token", token);		
-			LOGGER.debug("Nothing found for user, trying to generate token");
-			this.client.find(tokenCollection, query).onComplete(ar -> {
-				LOGGER.debug("Trying to retrieve message user data");
-				if (ar.succeeded() && (ar.result() == null || ar.result().isEmpty())) {
-					LOGGER.debug("Saving token.");
-					query.put(usernameField,username);
-					query.put("timestamp", System.currentTimeMillis());
-					client.save(tokenCollection, query)
-							.onSuccess(done -> {
-								fut.complete(token);
-							})
-							.onFailure(err -> {
-								fut.fail(err);
-							});
-	
-				} else {
-					// Token exists, generate a new one and try again					
-					LOGGER.debug("Retrying.");
-					generateUniqueToken(fut, userData);
-				}
-			});
-		})
-		.onFailure(err -> {
-			LOGGER.error(err);
-			fut.fail(err.getMessage());
-		});
+				.onSuccess(yay -> {
+					java.security.SecureRandom random = new java.security.SecureRandom();
+					byte[] tokenBytes = new byte[64];
+					random.nextBytes(tokenBytes);
+					String token = java.util.Base64.getUrlEncoder().withoutPadding().encodeToString(tokenBytes);
+					JsonObject query = new JsonObject().put("token", token);
+					LOGGER.debug("Nothing found for user, trying to generate token");
+					this.client.find(tokenCollection, query).onComplete(ar -> {
+						LOGGER.debug("Trying to retrieve message user data");
+						if (ar.succeeded() && (ar.result() == null || ar.result().isEmpty())) {
+							LOGGER.debug("Saving token.");
+							query.put(usernameField, username);
+							query.put("timestamp", System.currentTimeMillis());
+							client.save(tokenCollection, query)
+									.onSuccess(done -> {
+										fut.complete(token);
+									})
+									.onFailure(err -> {
+										fut.fail(err);
+									});
+
+						} else {
+							// Token exists, generate a new one and try again
+							LOGGER.debug("Retrying.");
+							generateUniqueToken(fut, userData);
+						}
+					});
+				})
+				.onFailure(err -> {
+					LOGGER.error(err);
+					fut.fail(err.getMessage());
+				});
 	}
+
 	/**
 	 * Test auth route
+	 * 
 	 * @param ctx the routing context
 	 */
-	public void request_reset(RoutingContext ctx)
-	{
-		RequestParameters params = ctx.get(ValidationHandler.REQUEST_CONTEXT_KEY);		
-		JsonObject body = params.body().getJsonObject();				
+	public void request_reset(RoutingContext ctx) {
+		RequestParameters params = ctx.get(ValidationHandler.REQUEST_CONTEXT_KEY);
+		JsonObject body = params.body().getJsonObject();
 		String emailOrUserName = body.getString("emailOrUserName");
 		LOGGER.debug("Received Password Reset request");
 		createOneTimeLoginAndSendMail(emailOrUserName)
-		.onFailure( err -> {
-			LOGGER.error(err.getMessage());
-			LOGGER.warn("Got a invalid request for a password reset for user " + emailOrUserName);
-		}
-		)
-		.onComplete(done -> {
-			LOGGER.debug("Request handled, returning");
-			// In any case we will respond with "request received" and not give out further information.
-			ctx.response().putHeader("content-type","text/plain");
-			ctx.response().end("Request received");
-		});
-				
-	}	
-	
+				.onFailure(err -> {
+					LOGGER.error(err.getMessage());
+					LOGGER.warn("Got a invalid request for a password reset for user " + emailOrUserName);
+				})
+				.onComplete(done -> {
+					LOGGER.debug("Request handled, returning");
+					// In any case we will respond with "request received" and not give out further
+					// information.
+					ctx.response().putHeader("content-type", "text/plain");
+					ctx.response().end("Request received");
+				});
+
+	}
+
 	public Future<Void> createOneTimeLoginAndSendMail(String userNameOrEmail) {
 		Promise<Void> promise = Promise.<Void>promise();
 		Promise<String> token_promise = Promise.<String>promise();
@@ -221,19 +219,19 @@ public class PasswordReset extends SoileAuthHandler{
 				.onFailure(err -> userPromise.fail(err));
 		return userPromise.future();
 	}
-	
+
 	@Override
 	public void authenticate(RoutingContext context, Handler<AsyncResult<User>> handler) {
 		HttpServerRequest request = context.request();
 		String token = request.getParam("token");
 		LOGGER.debug("Trying to authenticate a user with one time token " + token);
 		if (token == null) {
-			handler.handle(Future.failedFuture("Missing Token"));			
+			handler.handle(Future.failedFuture("Missing Token"));
 		} else {
 			getUserForToken(token)
 					.onSuccess(user -> {
-						LOGGER.debug("User obtained " );						
-						handler.handle(Future.succeededFuture(user));															
+						LOGGER.debug("User obtained ");
+						handler.handle(Future.succeededFuture(user));
 					})
 					.onFailure(err -> handler.handle(Future.failedFuture(err)));
 		}
@@ -243,14 +241,14 @@ public class PasswordReset extends SoileAuthHandler{
 		Promise<Void> promise = Promise.<Void>promise();
 		this.vertx.executeBlocking(future -> {
 			try {
-				String domain = SoileConfigLoader.getServerProperty("domain");				
+				String domain = SoileConfigLoader.getServerProperty("domain");
 				String subject = "SOILE Password Reset";
 				String from = "no-reply@" + domain;
 				String content = "Here is your one time login link for SOILE that you can use for a password reset.\n"
 						+ "https://" + domain + "/password_reset?token=" + token + "\n\n"
 						+ "Yours,\nSOILE Team\n";
 				Properties props = new Properties();
-				props.put("mail.smtp.host", domain);
+				props.put("mail.smtp.host", "localhost"); // we always send via the local mail server
 				props.put("mail.smtp.port", 25);
 				props.put("mail.smtp.auth", "false");
 				props.put("mail.smtp.starttls.enable", "false");
@@ -277,28 +275,26 @@ public class PasswordReset extends SoileAuthHandler{
 		return promise.future();
 
 	}
+
 	@Override
 	public void postAuthentication(RoutingContext ctx) {
 		// get the parameters once more and check, whether to store a cookie.
-		User user = ctx.user(); 						
+		User user = ctx.user();
 		LOGGER.debug("Post Authentication");
 		LOGGER.debug(user.principal().encodePrettily());
-		LOGGER.debug(user.principal().encodePrettily());				
-		// Finally set the token and send the reply		
-		jwtCreator.getToken(ctx).onSuccess(token ->
-		{
+		LOGGER.debug(user.principal().encodePrettily());
+		// Finally set the token and send the reply
+		jwtCreator.getToken(ctx).onSuccess(token -> {
 			LOGGER.debug("Token created, returning");
 			ctx.response().setStatusCode(200)
-			.putHeader(HttpHeaders.CONTENT_TYPE, "application/json; charset=utf-8")
-			.end(new JsonObject().put("token",token).encode());			
-		}).onFailure(fail ->
-		{
+					.putHeader(HttpHeaders.CONTENT_TYPE, "application/json; charset=utf-8")
+					.end(new JsonObject().put("token", token).encode());
+		}).onFailure(fail -> {
 			LOGGER.debug("Token creation failed");
-			if(fail.getCause() instanceof HttpException)
-			{
-				ctx.fail(((HttpException)fail.getCause()).getStatusCode());
+			if (fail.getCause() instanceof HttpException) {
+				ctx.fail(((HttpException) fail.getCause()).getStatusCode());
 			}
 		});
-		
+
 	}
 }
